4f9aee96c575f4144831d2e97b5cb559058e8f92
ChabanS <Chaban_s@ukr.net>
2016-01-24 12:40:43 +0200

    создали список категорий и категории

---
+++ src/ua/com/goit/Category.java
@@ -0,0 +1,17 @@
+package ua.com.goit;
+
+public class Category {
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public Category(String name) {
+		this.name = name;
+	}
+
+
+	
+}
--- src/ua/com/goit/Kickstarter.java
+++ src/ua/com/goit/Kickstarter.java
@@ -1,20 +1,40 @@
 package ua.com.goit;
 
-
+import java.util.Arrays;
 
 public class Kickstarter {
-
-	public static void main(String[] args) {
-		
-	Kickstarter app = new Kickstarter();
-	
-	app.run();
-	
+	private Categories categories;	
+	public Kickstarter(Categories categories2) {
+		this.categories=categories2;
 	}
 
+
+
+	public static void main(String[] args) {
+		Category category1 = new Category("Photo");
+		Category category2 = new Category("Video");
+		Category category3 = new Category("Music");
+		
+		Categories categories = new Categories();
+		categories.add(category1);
+		categories.add(category2);
+		categories.add(category3);
+	
+		Kickstarter app = new Kickstarter(categories);
+		app.run();
+		//System.out.println(Arrays.toString(categories.getCategories()));
+	}
+
+	
+
+	
 	private void run() {
 		QuoteGenerator quoteGenerator = new QuoteGenerator();
 		System.out.println(quoteGenerator.nextQuote());
+		
+		System.out.println();
+		System.out.println("Выберите категорию:");
+		System.out.println(Arrays.toString(categories.getCategories()));
 	}
 
 
