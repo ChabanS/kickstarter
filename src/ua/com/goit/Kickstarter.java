d2e3dc7d4c2d244a7f7abcf9635f6d1085d7e420
ChabanS <Chaban_s@ukr.net>
2016-01-29 17:09:37 +0200

    Рефокторинг

--- src/ua/com/goit/Kickstarter.java
+++ src/ua/com/goit/Kickstarter.java
@@ -12,58 +12,69 @@ public class Kickstarter {
 	}
 	
 
-	public static void main(String[] args) {
-		Category category1 = new Category("Photo");
-		Category category2 = new Category("Video");
-		Category category3 = new Category("Music");
-		
-		Categories categories = new Categories();
-		categories.add(category1);
-		categories.add(category2);
-		categories.add(category3);
-		
-		Project project1 = new Project("Фильмы про програмирование", 1000, 15, "описание фильма");
-		Project project2 = new Project("Фильмы про танцы", 5000, 10, "описание фильма");
-		
-		project1.setCategory(category2);
-		project2.setCategory(category2);
-		
-		
-		Projects projects = new Projects();
-		projects.add(project1);
-		projects.add(project2);
-		
-		Kickstarter app = new Kickstarter(categories, projects);
-		app.run();
-	}
-	
-	private void run() {
+	public void run() {
 		QuoteGenerator quoteGenerator = new QuoteGenerator();
 		System.out.println(quoteGenerator.nextQuote());
+		
 		while(true){
+			
+		askCategory();
+		
+		int categoryIndex = selectMenu();
+		
+		Category category = chooseCategory(categoryIndex);
+		
+		printProjects(category);
+		
+		
+		
+		}
+	}
+
+
+	private void printProjects(Category category) {
+		//получаем спимок проектов в даной категории
+		Project[] foundProgect = projects.getProjects(category);
+		
+		//выводим список проектов
+		for (Project project: foundProgect){
+			printProject(project);
+		}
+	}
+
+
+	private void printProject(Project project) {
+		System.out.println("Название проекта - " + project.getName());
+		System.out.println("Описание проекта - " + project.getDescription());
+		System.out.println("Сколько надо собрать - " + project.getAmount());
+		System.out.println("Сколько собрали - " + project.getExist());
+		System.out.println("Осталось дней - " + project.getDays());
+		System.out.println("---------------------------------");
+	}
+
+
+	private void askCategory() {
+		//предлагаем выбрать категорию
 		System.out.println();
 		System.out.println("Выберите категорию:");
 		System.out.println(Arrays.toString(categories.getCategories()));
-		
-		Scanner scanner = new Scanner(System.in);
-		int categoryIndex = scanner.nextInt();
+	}
+
+
+	private Category chooseCategory(int categoryIndex) {
+		//Выводим категорию
 		Category category=categories.getName(categoryIndex);
 		System.out.println("Вы выбрали категорию "+ category.getName());
 		System.out.println("---------------------------------");
-		Project[] foundProgect = projects.getProjects(category);
-//		for (int index=0; index<foundProgect.length; index++){
-//			System.out.println(foundProgect[index]);
-//		}
-		for (Project project: foundProgect){
-			System.out.println("Название проекта - " + project.getName());
-			System.out.println("Описание проекта - " + project.getDescription());
-			System.out.println("Сколько надо собрать - " + project.getAmount());
-			System.out.println("Сколько собрали - " + project.getExist());
-			System.out.println("Осталось дней - " + project.getDays());
-			System.out.println("---------------------------------");
+		return category;
+	}
 
-		}
-		}
+
+	private int selectMenu() {
+		//спрашиваем какуб категорию надо выбрать
+		Scanner scanner = new Scanner(System.in);
+		int categoryIndex = scanner.nextInt();
+		return categoryIndex;
 	}
 
 
---
+++ src/ua/com/goit/KickstarterRunner.java
@@ -0,0 +1,30 @@
+package ua.com.goit;
+
+public class KickstarterRunner {
+
+	public static void main(String[] args) {
+		Category category1 = new Category("Photo");
+		Category category2 = new Category("Video");
+		Category category3 = new Category("Music");
+		
+		Categories categories = new Categories();
+		categories.add(category1);
+		categories.add(category2);
+		categories.add(category3);
+		
+		Project project1 = new Project("Фильмы про програмирование", 1000, 15, "описание фильма");
+		Project project2 = new Project("Фильмы про танцы", 5000, 10, "описание фильма");
+		
+		project1.setCategory(category2);
+		project2.setCategory(category2);
+		
+		
+		Projects projects = new Projects();
+		projects.add(project1);
+		projects.add(project2);
+		
+		Kickstarter app = new Kickstarter(categories, projects);
+		app.run();
+	}
+
+}
